65f2c0415a2e654d54c5b761e80988a467175c90
Sébastien Le Callonnec <sebastien@weblogism.com>
2013-02-02 03:52:07 +0000

    Init project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HnStats::Application.config.secret_token = '11bc5bffb8c96c94360b725b0ee8f8e2b6f3598c17e4cd8ede1d6b8c9cca7d723e5b438039911ad6bebcf93b4e1d685af7cf98bdc3ebc9ef6bd3c23ebacc722f'
