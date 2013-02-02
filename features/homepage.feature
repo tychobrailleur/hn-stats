dddc810ecc1162005c5eec7ba12a146c8d48d479
Sébastien Le Callonnec <sebastien@weblogism.com>
2013-02-02 04:19:06 +0000

    Add Cucumber tests for homepage.

---
+++ features/homepage.feature
@@ -0,0 +1,5 @@
+Feature: Home Page
+
+Scenario: Display home page
+  Given a user visits the home page
+  Then he should see the site banner
---
+++ features/step_definitions/homepage_step.rb
@@ -0,0 +1,7 @@
+Given /^a user visits the home page$/ do
+  visit root_path
+end
+
+Then /^he should see the site banner$/ do
+  page.should have_selector(".container a.brand")
+end
